72c2b58339d12a0cf6c0794432aec2ad38296517
abetd <tadanori.abe@gmail.com>
2018-09-04 00:12:03 +0900

    除算の結果の配列数のテストを追加

--- src/test/java/com/example/money/AmountTest.java
+++ src/test/java/com/example/money/AmountTest.java
@@ -61,11 +61,13 @@ class AmountTest {
         Amount two = new Amount(2);
         Amount[] actual = two.divideAndRemainder(1);
         // TODO : equals() を実装後、assertArrayEquals で比較する
+        assertEquals(2, actual.length, "{商、剰余}");
         assertEquals(new Amount(2).value, actual[0].value, "2 / 1 = 2");
         assertEquals(new Amount(0).value, actual[1].value, "2 % 1 = 0");
 
         Amount four = new Amount(4);
         actual = four.divideAndRemainder(3);
+        assertEquals(2, actual.length, "{商、剰余}");
         assertEquals(new Amount(1).value, actual[0].value, "4 / 3 = 1");
         assertEquals(new Amount(1).value, actual[1].value, "4 % 3 = 1");
     }
