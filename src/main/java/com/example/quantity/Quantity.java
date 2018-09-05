febfbbd9a0fa2061f86be1d060cc260496e40ba7
baki504 <baki504@msn.com>
2018-09-06 06:44:22 +0900

    ピースと箱の換算ができるQuantity型の実装

--- src/main/java/com/example/quantity/Quantity.java
+++ src/main/java/com/example/quantity/Quantity.java
@@ -1,13 +1,42 @@
 package com.example.quantity;
 
+import com.example.quantity.unit.Unit;
+
 /**
  * 数量を扱う
  */
 public class Quantity {
     int value;
 
-    public Quantity(int value) {
+    Unit unit;
+
+    public Quantity(int value, Unit unit) {
         this.value = value;
+        this.unit = unit;
+    }
+
+    public Quantity add(Quantity other) {
+        int result = Math.addExact(value, opedand(other));
+        return new Quantity(result, unit);
+    }
+
+    public Quantity subtract(Quantity other) {
+        int result = Math.subtractExact(value, opedand(other));
+        return new Quantity(result, unit);
+    }
+
+    public Quantity multiply(int multiplier) {
+        int result = Math.multiplyExact(value, multiplier);
+        return new Quantity(result, unit);
+    }
+
+    private int opedand(Quantity quantity) {
+        if (unit.isEqualTo(quantity.unit))
+            return quantity.value;
+        else if (unit.isPiece())
+            return Math.multiplyExact(quantity.value, quantity.unit.piece());
+        else
+            return Math.floorDiv(quantity.value, unit.piece());
     }
 
     @Override
