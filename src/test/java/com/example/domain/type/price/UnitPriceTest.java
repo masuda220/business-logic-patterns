320b50cf23ce4a7407cd41ab368eedde1c960ada
Yu Takada <baki504@msn.com>
2018-09-10 23:06:16 +0900

    Issue #17 テストメソッドに名前を付与

--- src/test/java/com/example/domain/type/price/UnitPriceTest.java
+++ src/test/java/com/example/domain/type/price/UnitPriceTest.java
@@ -2,6 +2,7 @@ package com.example.domain.type.price;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import com.example.domain.type.money.Amount;
@@ -12,6 +13,7 @@ import com.example.domain.type.quantity.unit.PieceUnit;
 class UnitPriceTest {
 
     @Test
+    @DisplayName("ピース×ピースの合計金額")
     void multiplyPiece() {
         UnitPrice unitPrice = new UnitPrice(new Amount(95), new PieceUnit());
         Quantity quantity = new Quantity(24, new PieceUnit());
@@ -22,6 +24,7 @@ class UnitPriceTest {
     }
 
     @Test
+    @DisplayName("箱×箱の合計金額")
     void multiplyBox() {
         int piece = 24;
         UnitPrice unitPrice = new UnitPrice(new Amount(2280), new BoxUnit(piece));
@@ -33,6 +36,7 @@ class UnitPriceTest {
     }
 
     @Test
+    @DisplayName("ピース×箱の合計金額")
     void multiplyPieceAndBox() {
         int piece = 24;
         UnitPrice unitPrice = new UnitPrice(new Amount(95), new PieceUnit());
@@ -44,12 +48,13 @@ class UnitPriceTest {
     }
 
     @Test
+    @DisplayName("箱×ピースで例外")
     void multiplyBoxAndPiece() {
         int piece = 24;
         UnitPrice unitPrice = new UnitPrice(new Amount(2280), new BoxUnit(piece));
         Quantity quantity = new Quantity(5, new PieceUnit());
 
-        assertThrows(IllegalArgumentException.class, () -> unitPrice.multiply(quantity), "ピース・箱の乗算で例外");
+        assertThrows(IllegalArgumentException.class, () -> unitPrice.multiply(quantity));
     }
 
 }
