9832099a0550bca0298689c15a8092c27503cd9d
abetd <tadanori.abe@gmail.com>
2018-10-10 22:40:56 +0900

    #62 Eventの不要になった変数、メソッドを削除

--- src/main/java/com/example/domain/model/gate/Event.java
+++ src/main/java/com/example/domain/model/gate/Event.java
@@ -1,23 +1,11 @@
 package com.example.domain.model.gate;
 
-import static com.example.domain.model.gate.State.*;
-
 /**
  * イベントを扱う
  */
 public enum Event {
-    OPEN(OPENED),
-    CLOSE(CLOSED),
-    LOCK(LOCKED),
-    UNLOCK(CLOSED);
-
-    State nextState;
-
-    Event(State nextState) {
-        this.nextState = nextState;
-    }
-
-    State next() {
-        return nextState;
-    }
+    OPEN,
+    CLOSE,
+    LOCK,
+    UNLOCK
 }
