80966100840310e729b3439309709c603b6ba823
abetd <tadanori.abe@gmail.com>
2018-10-30 22:58:52 +0900

    #70 hourパッケージのテスト修正とワーニングの対応

--- src/test/java/com/example/domain/type/hour/MinuteTest.java
+++ src/test/java/com/example/domain/type/hour/MinuteTest.java
@@ -11,27 +11,19 @@ class MinuteTest {
 
     @Test()
     @DisplayName("時間の引き算テスト")
-    void subtructText() {
-        try {
-            Minute testMin = new Minute(50);
-            Minute targetMin = new Minute(40);
-            targetMin.subtract(testMin);
-            //40分から50分を引くのでエラーがThrowされる
-            //fail();
-        }
-        catch(DateTimeException e) {
-            assertEquals(e.getMessage(), "Error of minus time.");
-        }
+    void subtract() {
+        Minute minute23 = new Minute(23);
+        Minute minute40 = new Minute(40);
+        Minute actual = minute40.subtract(minute23);
+        assertEquals(17, actual.value);
+    }
 
-        try {
-            Minute testMin = new Minute(23);
-            Minute targetMin = new Minute(40);
-            targetMin.subtract(testMin);
-            //40分から23分を引くので0以上であれば正しい
-            assertTrue(targetMin.value >= 0);
-        }
-        catch(DateTimeException e) {
-            //fail();
-        }
+    @Test()
+    @DisplayName("マイナスになる時間の引き算で例外")
+    void subtractFailure() {
+        Minute minute23 = new Minute(23);
+        Minute minute40 = new Minute(40);
+        Throwable exception = assertThrows(DateTimeException.class, () -> minute23.subtract(minute40));
+        assertEquals("Error of minus time.", exception.getMessage());
     }
 }
